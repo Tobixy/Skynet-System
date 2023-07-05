4c673f1253315fc3a915adbc137fe4654dd29b64
Tobixy <shivamrajput69bs@gmail.com>
2023-07-05 13:50:03 +0530

    Update config.py

--- Skynet_System/config.py
+++ Skynet_System/config.py
@@ -3,9 +3,9 @@ import Skynet_System.config as Config
 API_ID: int = 29536634
 API_HASH: str = "f6d57b4713f0ecae0ac2b1fb69e136e5"
 STRING_SESSION: str = "1BVtsOJMBuzKr7bB1VVSDm-i2bC6PutIDylBPMevm2RHOmDuysE-xu06blH_E3KDXQmEuxvN9yvMpvB2NLBoPpODu1lQI1PXjBkyXKuEcLorkDTcmDhHsqkjVh9biTW73Q8Mwnbh4DvhbN"
-Skynet_logs: Union[str, int] = -1001293004442
-Skynet_approved_logs: Union[str, int] = -1001274776877
-GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+Skynet_logs: [str, int] = -1001293004442
+Skynet_approved_logs: [str, int] = -1001274776877
+GBAN_MSG_LOGS: Optional[str, int]] = None
 BOT_TOKEN: str = "6339781130:AAEw2dHjSrkKaytBHDXBzr5Y9c2lkhZgLRw"
 Skynet = data["SKYNET"]
 INSPECTORS = data["5715764478"]
