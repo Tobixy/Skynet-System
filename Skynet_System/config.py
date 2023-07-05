a3db33f25fee57e6525197b45e63b7cdc5a3e100
Tobixy <shivamrajput69bs@gmail.com>
2023-07-05 13:47:24 +0530

    Update config.py

--- Skynet_System/config.py
+++ Skynet_System/config.py
@@ -1,15 +1,16 @@
 import Skynet_System.config as Config
 
-
-  # REQUIRED
-    API_ID_KEY = "29536634"
-    API_HASH_KEY = "f6d57b4713f0ecae0ac2b1fb69e136e5"  
-    STRING_SESSION = "1BVtsOJMBuzKr7bB1VVSDm-i2bC6PutIDylBPMevm2RHOmDuysE-xu06blH_E3KDXQmEuxvN9yvMpvB2NLBoPpODu1lQI1PXjBkyXKuEcLorkDTcmDhHsqkjVh9biTW73Q8Mwnbh4DvhbNlmhkXzkV7TZjINH655oRgCuSddnAom5GPmzTiJMgaE8v9UUAL9qa_6SCEcGTInHGBWRRtZidbuPunBce-6KAG0By064u0rtL1gHluDgNkCDevuo3kX8pnm4ILmgSazCHN29Pm_RaIEiPM66uch9lfxPM2uHJGdWoBE4v_X9NCC8z9ph0JbX3rpv69ulo6Gv94dEC2abJZrlVibTFiU="
-    Skynet = "SKYNET"
-    ENFORCERS = "5715764478 5348193047"
-    Skynet_logs = "-1001835086294"
-    Skynet_approved_logs = "-1001835086294"
-    GBAN_MSG_LOGS = "-1001835086294"
-    INSPECTOR = "5715764478 5348193047"
-    MONGO_URL = "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"
-    DEVELOPER = "5715764478 5348193047"
+API_ID: int = 29536634
+API_HASH: str = "f6d57b4713f0ecae0ac2b1fb69e136e5"
+STRING_SESSION: str = "1BVtsOJMBuzKr7bB1VVSDm-i2bC6PutIDylBPMevm2RHOmDuysE-xu06blH_E3KDXQmEuxvN9yvMpvB2NLBoPpODu1lQI1PXjBkyXKuEcLorkDTcmDhHsqkjVh9biTW73Q8Mwnbh4DvhbN"
+Skynet_logs: Union[str, int] = -1001293004442
+Skynet_approved_logs: Union[str, int] = -1001274776877
+GBAN_MSG_LOGS: Optional[Union[str, int]] = None
+BOT_TOKEN: str = "6339781130:AAEw2dHjSrkKaytBHDXBzr5Y9c2lkhZgLRw"
+Skynet = data["SKYNET"]
+INSPECTORS = data["5715764478"]
+DEVELOPERS = data["5715764478"]
+MONGO_DB_URL : str
+"mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority"
+   
+   
